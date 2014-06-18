59c8b0d65b46ac84df6bd66ed09d716b3e616df0
jlam732 <charon732@gmail.com>
2014-06-18 13:10:31 -0400

    added calls for rfid and device

--- src/APIClient.java
+++ src/APIClient.java
@@ -5,7 +5,10 @@ import java.util.TimerTask;
 import java.util.TreeMap;
 
 import com.bluerover.api.BlueroverApi;
-import com.bluerover.api.Result;
+import com.bluerover.model.Device;
+import com.bluerover.model.Event;
+import com.bluerover.model.Result;
+import com.bluerover.model.Rfid;
 
 public class APIClient {
 
@@ -23,6 +26,14 @@ public class APIClient {
 		timer.scheduleAtFixedRate(new callEventTask(), 0, seconds * 1000);
 	}
 
+	public void scheduleDeviceCall(int seconds) {
+		timer.scheduleAtFixedRate(new callDeviceTask(), 0, seconds * 1000);
+	}
+	
+	public void scheduleRfidCall(int seconds) {
+		timer.scheduleAtFixedRate(new callRfidTask(), 0, seconds * 1000);
+	}
+	
 	class callEventTask extends TimerTask {
 
 		@Override
@@ -34,11 +45,41 @@ public class APIClient {
 			times.put("start_time", Objects.toString(startTime, null));
 			times.put("end_time", Objects.toString(endTime, null));
 			times.put("page", "0");
-			Result result = null;
+			Result<Event> result = null;
 			try {
 				result = api.getEvents(Objects.toString(
 						System.currentTimeMillis() / 1000L - 10 * 1000, null), Objects
 						.toString(System.currentTimeMillis() / 1000L, null), "0");
+			} catch (IOException e) {
+				System.err.println("Error with connecting to server");
+				e.printStackTrace();
+			}
+			System.out.println(result);
+		}
+	}
+	
+	class callDeviceTask extends TimerTask {
+
+		@Override
+		public void run() {
+			Result<Device> result = null;
+			try {
+				result = api.getDevices();
+			} catch (IOException e) {
+				System.err.println("Error with connecting to server");
+				e.printStackTrace();
+			}
+			System.out.println(result);
+		}
+	}
+	
+	class callRfidTask extends TimerTask {
+
+		@Override
+		public void run() {
+			Result<Rfid> result = null;
+			try {
+				result = api.getRfids();
 			} catch (IOException e) {
 				System.err.println("Error with connecting to server");
 				e.printStackTrace();
@@ -54,6 +95,8 @@ public class APIClient {
 		creds.put("baseURL", "http://developers.bluerover.us");
 		APIClient client = new APIClient(creds);
 		
-		client.scheduleEventCall(10);
+		//client.scheduleEventCall(10);
+		//client.scheduleDeviceCall(5);
+		client.scheduleRfidCall(5);
 	}
 }
